32e721d8a2108d6985fe15e14c95fe6db2222f6f
carloshnunes <110049855+carloshnunes@users.noreply.github.com>
2024-06-09 19:28:52 -0300

    atualização página carrinho para mostrar quantidade e valor certo

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -3,22 +3,37 @@ import { CartContext, totalFromCart } from '../context/CartContext';
 import './CartPage.css';
 
 const CartPage = () => {
-  const { cart, addToCart, removeFromCart } = useContext(CartContext);
+  const { cart } = useContext(CartContext);
+
+  function groupProductsById(cart) {
+    const grouped = cart.reduce((acc, product) => {
+      if (!acc[product.id]) {
+        acc[product.id] = {
+          id: product.id,
+          name: product.name,
+          price: product?.price.finalPrice || 0,
+          count: 0,
+        };
+      }
+      acc[product.id].count += 1;
+      return acc;
+    }, {});
+
+    return Object.values(grouped);
+  }
+  const groupedCart = groupProductsById(cart);
+  console.log(groupedCart, 'groupedCart');
 
   return (
     <div className="cart-page">
       <h2>Seu Carrinho</h2>
-      {cart.map((item) => (
+      {groupedCart.map((item) => (
         <div key={item.id} className="cart-item">
-          <img src={item.image} alt={item.name} className="cart-item-image" />
           <div className="cart-item-details">
             <h3>{item.name}</h3>
-            <p>Preço: R$ {item.price.finalPrice.toFixed(2)}</p>
-            <div className="cart-item-quantity">
-              <button onClick={() => removeFromCart(item.id)}>-</button>
-              <span>{item.quantity}</span>
-              <button onClick={() => addToCart(item)}>+</button>
-            </div>
+            <p>
+              {item.count} un - R$ {item.price * item.count}
+            </p>
           </div>
         </div>
       ))}
