a6b0839693bc56336a8eec2395aba09efd675bac
carloshnunes <110049855+carloshnunes@users.noreply.github.com>
2024-06-10 14:33:44 -0300

    adição sidebar e mudanças design

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -3,6 +3,7 @@ import { CartContext } from '../context/CartContext';
 import products from '../data/products.json';
 import ProductList from '../components/ProductList';
 import './ProductsPage.css';
+import searchIcon from '../components/images/search-icon.png';
 
 const ProductsPage = () => {
   const { addToCart, removeFromCart, cart } = useContext(CartContext);
@@ -18,32 +19,67 @@ const ProductsPage = () => {
 
   return (
     <div className="products-page">
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Pesquisar"
-          value={searchTerm}
-          onChange={handleSearch}
-          className="search-bar"
+      <div className="main-content">
+        <div className="search-container">
+          <img src={searchIcon} alt="Search" className="search-icon" />
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            value={searchTerm}
+            onChange={handleSearch}
+            className="search-bar"
+          />
+          <select className="order-select">
+            <option value="name-asc">Nome (A - Z)</option>
+            <option value="name-desc">Nome (Z - A)</option>
+            <option value="price-asc">Preço (Baixo para Alto)</option>
+            <option value="price-desc">Preço (Alto para Baixo)</option>
+          </select>
+          <select className="display-select">
+            <option value="10">Exibição: 10</option>
+            <option value="20">Exibição: 20</option>
+            <option value="50">Exibição: 50</option>
+          </select>
+        </div>
+        <ProductList
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+          products={filteredProducts}
+          cart={cart}
         />
-        <select className="order-select">
-          <option value="name-asc">Nome (A - Z)</option>
-          <option value="name-desc">Nome (Z - A)</option>
-          <option value="price-asc">Preço (Baixo para Alto)</option>
-          <option value="price-desc">Preço (Alto para Baixo)</option>
-        </select>
-        <select className="display-select">
-          <option value="10">Exibição: 10</option>
-          <option value="20">Exibição: 20</option>
-          <option value="50">Exibição: 50</option>
-        </select>
       </div>
-      <ProductList
-        addToCart={addToCart}
-        removeFromCart={removeFromCart}
-        products={filteredProducts}
-        cart={cart}
-      />
+      <div className="sidebar">
+        <h3 className="filterHeader">Ações Comerciais</h3>
+        <div className="filter-group">
+          <label>
+            <input type="checkbox" /> Em Promoção
+          </label>
+          <label>
+            <input type="checkbox" /> Em Oferta
+          </label>
+        </div>
+        <h3 className="filterHeader">Estoque</h3>
+        <div className="filter-group">
+          <label>
+            <input type="checkbox" /> Estoque Disponível
+          </label>
+        </div>
+        <h3 className="filterHeader">Categorias</h3>
+        <div className="filter-group">
+          <label>
+            <input type="checkbox" /> Genéricos
+          </label>
+          <label>
+            <input type="checkbox" /> Similares
+          </label>
+          <label>
+            <input type="checkbox" /> Psicotrópicos
+          </label>
+          <label>
+            <input type="checkbox" /> Higiene e Beleza
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
