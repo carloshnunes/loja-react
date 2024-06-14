6a83e7c1a28ba889a17a7097a37aa7fe5c20c604
carloshnunes <110049855+carloshnunes@users.noreply.github.com>
2024-06-14 15:46:09 -0300

    ajustes finais na `checkoutPage.js`

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -7,6 +7,13 @@ import { Link } from 'react-router-dom';
 const CheckoutPage = () => {
   const { cart } = useContext(CartContext);
 
+  const calculateTotalWithTax = (cart, tax) => {
+    const total = totalFromCart(cart);
+    return parseFloat(total) + parseFloat(tax);
+  };
+
+  const TAX_AMOUNT = 10.0;
+
   return (
     <div className="checkout-container">
       <div className="menu-header-checkout">
@@ -20,15 +27,21 @@ const CheckoutPage = () => {
       <div className="checkout-main">
         <div className="checkout-details">
           <div className="checkout-section">
-            {/* Opções de pagamento */}
             <h2 className="titles-checkout">OPÇÃO DE PAGAMENTO</h2>
-            <p>O valor total dos produtos é de R$ 460,00 (com impostos).</p>
-            <p>Selecione a forma de pagamento:</p>
-            <label htmlFor="payment-method"></label>
-            <select id="payment-method">
-              <option value="">Selecione a forma de pagamento</option>
-              <option value="boleto">Boleto</option>
-            </select>
+            <p>
+              O valor total dos produtos é de :R$
+              {calculateTotalWithTax(cart, TAX_AMOUNT).toFixed(2)} (com
+              impostos).
+            </p>
+            <div className="payment-bar">
+              <p>Selecione a forma de pagamento:</p>
+              <label htmlFor="payment-method"></label>
+              <select id="payment-method">
+                <option value="">Selecione a forma de pagamento</option>
+                <option value="boleto">Boleto</option>
+              </select>
+            </div>
+            <button className="botao-boleto">Boleto</button>
           </div>
           <div className="checkout-section">
             <h2 className="titles-checkout">ENDEREÇO DE ENTREGA</h2>
@@ -42,10 +55,13 @@ const CheckoutPage = () => {
         </div>
         <div className="checkout-sidebar">
           <h2 className="checkout-sidebar-header">VALORES</h2>
-          <p>Total (sem impostos): R${totalFromCart(cart)}</p>
-          <p>Impostos: R$ 10,00</p>
-          <p></p>
-          <button className="submit-order">Enviar Pedido{Checkout}</button>
+          <div className="content-sidebar-checkout">
+            <p>Total (sem impostos): R${totalFromCart(cart)}</p>
+            <p>Impostos: R$ {TAX_AMOUNT.toFixed(2)}</p>
+          </div>
+          <div className="total-sidebar-checkout">
+            Total: {calculateTotalWithTax(cart, TAX_AMOUNT).toFixed(2)}
+          </div>
         </div>
       </div>
     </div>
